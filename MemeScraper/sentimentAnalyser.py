e664af577c9e13482e92b636fb0c320d3c428c72
ilijagrbic <ilijagrb.ns@gmail.com>
2019-01-25 12:45:47 +0100

    NLTK-VADER odradjen

---
+++ MemeScraper/sentimentAnalyser.py
@@ -0,0 +1,26 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import csv
+import os
+import collections
+import smart_open
+import random
+
+with open('memegenerator1.csv', 'r', encoding="utf-8") as f:
+  reader = csv.reader(f)
+  your_list = list(reader)
+
+sid = SentimentIntensityAnalyzer()
+
+for idx, row in enumerate(your_list):
+    if idx%2==0:
+        ss = sid.polarity_scores(row[1])
+        for k in ss:
+            row.append(ss[k])
+
+with open("meme1sentiment.csv", 'a', encoding="utf-8") as csv_file:
+    writer = csv.writer(csv_file)
+    for idx, item in enumerate(your_list):
+        if idx % 2 == 0:
+            writer.writerow(item)
+            print(item)
