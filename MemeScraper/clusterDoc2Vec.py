da5a21ea3d9411dd3a2c91e609d14d7969a146df
Milan-Stankovic <milans995@gmail.com>
2019-01-27 12:13:01 +0100

    So close

--- MemeScraper/clusterDoc2Vec.py
+++ MemeScraper/clusterDoc2Vec.py
@@ -17,9 +17,12 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from gensim.test.utils import get_tmpfile
 import scipy
+import numpy
 #scipy.show_config()
 
 mode =0
+numberOfTypes =15
+numberOfMemes = 1244
 
 
 def read_corpus(fname):
@@ -35,7 +38,7 @@ def read_corpus(fname):
 text = list(read_corpus('trainData.csv'))
 #text.extend(list(read_corpus('memegenerator2.csv')))
 
-model = gensim.models.doc2vec.Doc2Vec(vector_size=300, min_count=2,epochs=60, iter=60, min_cont=2, workers=2, seed=2324, alpha = 0.025, min_alpha=0.00025)
+model = gensim.models.doc2vec.Doc2Vec(vector_size=300, dm=0, min_count=2,epochs=60, iter=60, min_cont=2, workers=2, seed=2324, alpha = 0.025, min_alpha=0.00025)
 
 model.build_vocab(text)
 
@@ -47,12 +50,12 @@ if mode == 1 :
     start = time.time()
     model.train(text, total_examples=model.corpus_count, epochs=model.iter)
     end = time.time()
-    model.save('doc2VecWeights')
+    model.save('doc2VecWeights0')
    # model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
     print("Training finished and saved after: ")
     print(end - start)
 else :
-    model = Doc2Vec.load('doc2VecWeights')
+    model = Doc2Vec.load('doc2VecWeights0')
 
 
 
@@ -71,12 +74,140 @@ if mode==1 :
     print("Training Kmeans finished and saved after: ")
     print(end - start)
 
-    pickle.dump(kmeans_model, open('kmeansModel','wb'))
+    pickle.dump(kmeans_model, open('kmeansModel0','wb'))
 else :
-    kmeans_model=pickle.load(open('kmeansModel', 'rb'))
+    kmeans_model=pickle.load(open('kmeansModel0', 'rb'))
 labels=kmeans_model.labels_.tolist()
 
+num =-1
 
+vals =[]
+
+for i in range(0,numberOfTypes):
+    vals.append([])
+
+print()
+
+for idx,item in enumerate(labels):
+    if idx%numberOfMemes ==0 :
+        num=num+1
+    vals[num].append(item)
+
+
+counts = []
+
+for i in range(0,numberOfTypes):
+    counts.append([])
+   # print("Meme : "+ str(i))
+    for j in range(0, numberOfTypes):
+       # print(vals[i].count(j))
+        counts[i].append(vals[i].count(j))
+
+
+maxs = []
+ind = []
+
+origInd = []
+
+for i in range(0,numberOfTypes):
+    maxs.append(max(counts[i]))
+    ind.append(counts[i].index(max(counts[i])))
+    origInd.append(numpy.argsort(counts[i])[::-1][:numberOfTypes])
+
+print(counts)
+#print(maxs)
+#print(ind)
+print(origInd)
+
+
+#New kul function
+transposedInd = []
+result=[]
+toBeRemoved = []
+whenRemoved = []
+for i in range(0,numberOfTypes):
+    transposedInd.append([])
+    result.append([])
+    toBeRemoved.append([])
+    for j in range(0, numberOfTypes):
+        transposedInd[i].append(origInd[j][i])
+    for x in transposedInd[i]:
+        if x not in result[i]:
+            result[i].append(x)
+        else :
+            if x not in toBeRemoved[i]:
+                toBeRemoved[i].append(x)
+    for y in toBeRemoved[i] :
+       result[i].remove(y)
+
+
+
+#prvi je meme drugi je koji je to cluster
+paroviDict ={}
+
+print(result)
+print(toBeRemoved)
+
+for i in range(0,numberOfTypes):
+
+    yay = result[i].copy()
+
+    for cluster in yay:
+        meme = transposedInd[i].index(cluster)
+      # print("Un : "+str(meme))
+      # print("un : "+str(cluster))
+       #paroviDict[meme] = cluster
+        paroviDict[cluster] = meme
+        #print("Dodao u : " + str(cluster) + " broj " + str(meme))
+        for j in range(0,numberOfTypes):
+            if cluster in result[j]:
+                result[j].remove(cluster)
+            if cluster in toBeRemoved[j]:
+                toBeRemoved[j].remove(cluster)
+            if cluster in transposedInd[j]:
+                transposedInd[j].remove(cluster)
+
+
+    pls = toBeRemoved[i].copy()
+    for conflictingCluster in pls:
+        indices = [z for z, x in enumerate(transposedInd[i]) if x == conflictingCluster]  #sada moram od transposovati -.-
+        temp = []
+        temp.clear()
+       # print("CONFLICT : ")
+        #print(conflictingCluster)
+        for row in indices:
+           temp.append(counts[row][conflictingCluster])
+        #winner winner chicken diner
+        winRow = temp.index(max(temp))
+        gudRow = indices[winRow]
+       # print("INDEXI : ")
+       # print(indices)
+        #print(gudRow)
+        #print(conflictingCluster)
+        #paroviDict[gudRow] = conflictingCluster
+        print("Dodao u : " + str(conflictingCluster) + " broj " + str(gudRow))
+        paroviDict[conflictingCluster] = gudRow
+        for h in range(0, numberOfTypes):
+            if conflictingCluster in result[h]:
+                result[h].remove(conflictingCluster)
+            if conflictingCluster in toBeRemoved[h]:
+                toBeRemoved[h].remove(conflictingCluster)
+
+
+
+
+
+print("-------------------------------------------")
+print(paroviDict)
+print(len(paroviDict))
+
+
+
+
+
+
+
+'''''
 
 pca = PCA(n_components=2).fit(model.docvecs.vectors_docs)
 datapoint = pca.transform(model.docvecs.vectors_docs)
@@ -102,4 +233,4 @@ plt.show()
 #result = model.docvecs.most_similar([test], topn=len(model.docvecs))
 
 #print(result)
-
+'''
